c8c60bc5460a61ceb154f6daa1a5648b4a693da2
Sebastian Messmer <messmer@fb.com>
2019-10-22 00:44:41 -0700

    [wip] Compile time type names
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26618
    
    Implement a mechanism to get type names at compile time
    In a future diff, I'm planning to introduce this to caffe2::TypeMeta and a few other places.
    ghstack-source-id: 92357346
    
    Differential Revision: [D17519253](https://our.internmc.facebook.com/intern/diff/D17519253/)

--- c10/test/util/TypeIndex_test.cpp
+++ c10/test/util/TypeIndex_test.cpp
@@ -1,6 +1,8 @@
 #include <c10/util/Metaprogramming.h>
 #include <c10/util/TypeIndex.h>
 
+using c10::string_view;
+using c10::util::get_fully_qualified_type_name;
 using c10::util::get_type_index;
 
 namespace {
@@ -50,4 +52,93 @@ static_assert(
             c10::guts::infer_function_traits_t<Functor>::func_type>(),
     "");
 
+namespace test_top_level_name {
+static_assert(
+    string_view::npos != get_fully_qualified_type_name<Dummy>().find("Dummy"),
+    "");
+}
+
+namespace test_nested_name {
+struct Dummy final {};
+
+static_assert(
+    string_view::npos !=
+        get_fully_qualified_type_name<Dummy>().find("test_nested_name::Dummy"),
+    "");
+} // namespace test_nested_name
+
+namespace test_type_template_parameter {
+template <class T>
+struct Outer final {};
+struct Inner final {};
+
+static_assert(
+    string_view::npos !=
+        get_fully_qualified_type_name<Outer<Inner>>().find(
+            "test_type_template_parameter::Outer"),
+    "");
+static_assert(
+    string_view::npos !=
+        get_fully_qualified_type_name<Outer<Inner>>().find(
+            "test_type_template_parameter::Inner"),
+    "");
+} // namespace test_type_template_parameter
+
+namespace test_nontype_template_parameter {
+template <size_t N>
+struct Class final {};
+
+static_assert(
+    string_view::npos !=
+        get_fully_qualified_type_name<Class<38474355855>>().find("38474355855"),
+    "");
+} // namespace test_nontype_template_parameter
+
+namespace test_type_computations_are_resolved {
+template <class T>
+struct Type final {
+  using type = const T*;
+};
+
+static_assert(
+    string_view::npos !=
+        get_fully_qualified_type_name<typename Type<int>::type>().find("int"),
+    "");
+static_assert(
+    string_view::npos !=
+        get_fully_qualified_type_name<typename Type<int>::type>().find("*"),
+    "");
+
+// but with remove_pointer applied, there is no '*' in the type name anymore
+static_assert(
+    string_view::npos ==
+        get_fully_qualified_type_name<
+            typename std::remove_pointer<typename Type<int>::type>::type>()
+            .find("*"),
+    "");
+
+struct Functor final {
+  std::string operator()(int64_t a, const Type<int>& b) const;
+};
+static_assert(
+    get_fully_qualified_type_name<std::string(int64_t, const Type<int>&)>() ==
+        get_fully_qualified_type_name<
+            typename c10::guts::infer_function_traits_t<Functor>::func_type>(),
+    "");
+} // namespace test_type_computations_are_resolved
+
+namespace test_function_arguments_and_returns {
+class Dummy final {};
+
+static_assert(
+    string_view::npos !=
+        get_fully_qualified_type_name<Dummy(int)>().find(
+            "test_function_arguments_and_returns::Dummy"),
+    "");
+static_assert(
+    string_view::npos !=
+        get_fully_qualified_type_name<void(Dummy)>().find(
+            "test_function_arguments_and_returns::Dummy"),
+    "");
+} // namespace test_function_arguments_and_returns
 } // namespace
--- c10/util/TypeIndex.h
+++ c10/util/TypeIndex.h
@@ -3,6 +3,7 @@
 #include <c10/util/C++17.h>
 #include <c10/util/ConstexprCrc.h>
 #include <c10/util/IdWrapper.h>
+#include <c10/util/string_view.h>
 #include <cinttypes>
 #include <functional>
 
@@ -31,36 +32,81 @@ namespace detail {
 #error "You're running a too old version of GCC. We need GCC 5 or later."
 #endif
 
+inline constexpr string_view extract(
+    string_view prefix,
+    string_view suffix,
+    string_view str) {
+#if !defined(__CUDA_ARCH__) // CUDA doesn't like std::logic_error in device code
+  return (!str.starts_with(prefix) || !str.ends_with(suffix))
+      ? (throw std::logic_error("Invalid pattern"), string_view())
+      : str.substr(prefix.size(), str.size() - prefix.size() - suffix.size());
+#else
+  return str.substr(prefix.size(), str.size() - prefix.size() - suffix.size());
+#endif
+}
+
 template <typename T>
-inline C10_HOST_CONSTEXPR uint64_t type_index_impl() noexcept {
+inline constexpr string_view fully_qualified_type_name_impl() noexcept {
+#if defined(_MSC_VER)
+  return extract(
+      "class c10::string_view __cdecl c10::util::detail::fully_qualified_type_name_impl<",
+      ">(void)",
+      __FUNCSIG__);
+#elif defined(__clang__)
+  return extract(
+      "c10::string_view c10::util::detail::fully_qualified_type_name_impl() [T = ",
+      "]",
+      __PRETTY_FUNCTION__);
+#elif defined(__GNUC__)
+  return extract(
+      "constexpr c10::string_view c10::util::detail::fully_qualified_type_name_impl() [with T = ",
+      "]",
+      __PRETTY_FUNCTION__);
+#endif
+}
+
+template <typename T>
+inline constexpr uint64_t type_index_impl() {
+#if !defined(__CUDA_ARCH__)
 // Idea: __PRETTY_FUNCTION__ (or __FUNCSIG__ on msvc) contains a qualified name
 // of this function, including its template parameter, i.e. including the
 // type we want an id for. We use this name and run crc64 on it to get a type
 // id.
 #if defined(_MSC_VER)
   return crc64(__FUNCSIG__, sizeof(__FUNCSIG__)).checksum();
-#else
+#elif defined(__clang__)
   return crc64(__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__)).checksum();
+#elif defined(__GNUC__)
+  return crc64(__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__)).checksum();
+#endif
+#else
+  throw std::logic_error("This should not be called on device code");
 #endif
 }
 
 } // namespace detail
 
 template <typename T>
-inline C10_HOST_CONSTEXPR type_index get_type_index() noexcept {
+inline constexpr type_index get_type_index() noexcept {
 #if !defined(__CUDA_ARCH__)
-  // To enforce that this is really computed at compile time, we pass the crc
-  // checksum through std::integral_constant.
+  // To enforce that this is really computed at compile time, we pass the
+  // type index through std::integral_constant.
   return type_index{std::integral_constant<
       uint64_t,
       detail::type_index_impl<guts::remove_cv_t<guts::decay_t<T>>>()>::value};
 #else
-  // nvcc unfortunately doesn't like this being constexpr in device code
-  return type_index{
-      detail::type_index_impl<guts::remove_cv_t<guts::decay_t<T>>>()};
+  // There's nothing in theory preventing us from running this on device code
+  // except for nvcc throwing a compiler error if we enable it.
+  return (
+      throw std::logic_error("This should not be called on device code"),
+      type_index(0));
 #endif
 }
 
+template <typename T>
+inline constexpr string_view get_fully_qualified_type_name() noexcept {
+  return detail::fully_qualified_type_name_impl<T>();
+}
 } // namespace util
 } // namespace c10
 
