61f3b4f52aef0e61256104e90ccc732fd27aa3b6
albanD <desmaison.alban@gmail.com>
2019-11-04 11:23:58 -0800

    Handle warning in torchscript
    ghstack-source-id: 027e3fad9cb05bf8fb9b04df2fcffd33b7a05950
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27154

--- torch/csrc/jit/script/init.cpp
+++ torch/csrc/jit/script/init.cpp
@@ -659,6 +659,7 @@ void initJitScriptBindings(PyObject* module) {
       .def(
           "__call__",
           [](py::args args, py::kwargs kwargs) {
+            HANDLE_TH_ERRORS
             // see: [pybind11 varargs]
             auto strongPtr = py::cast<StrongFunctionPtr>(args[0]);
             Function& callee = *strongPtr.function_;
@@ -672,6 +673,7 @@ void initJitScriptBindings(PyObject* module) {
               tracer::getTracingState()->graph->pop_scope();
             }
             return result;
+            END_HANDLE_TH_ERRORS_PYBIND
           })
       .def(
           "save",
