834c2051a7deca9416b4ae7fe3b80b35282268f9
James Reed <jamesreed@fb.com>
2019-11-04 18:44:39 -0800

    [WIP][JIT] Trace module calls
    ghstack-source-id: 012d6bb8988e3c7f5dcb753ff75f77b0de5746f4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/28474

--- torch/csrc/jit/ir.h
+++ torch/csrc/jit/ir.h
@@ -637,11 +637,12 @@ struct TORCH_API Node {
   virtual ~Node() = default;
 
   // Methods for accessing attributes
-  void copyAttributes(const Node& rhs) {
+  Node* copyAttributes(const Node& rhs) {
     values_.clear();
     for (const AVPtr& i : rhs.values_) {
       values_.push_back(i->clone());
     }
+    return this;
   }
   bool hasAttribute(Symbol name) const {
     AT_ASSERT(name.is_attr());
@@ -910,6 +911,9 @@ struct Block {
   void eraseOutput(size_t i) {
     output_->removeInput(i);
   }
+  void replaceOutput(size_t i, Value* n) {
+    output_->replaceInput(i, n);
+  }
   void permuteOutputs(const std::vector<size_t>& new_inputs) {
     output_->permuteInputs(new_inputs);
   }
@@ -1031,12 +1035,9 @@ struct Graph {
     return unique_names_;
   }
 
-  void push_scope(const std::string& scope_name) {
-    current_scope_ = current_scope_->push(Symbol::scope(scope_name));
-  }
-  void pop_scope() {
-    current_scope_ = current_scope_->parent();
-  }
+  TORCH_API void push_scope(const std::string& scope_name);
+  TORCH_API void pop_scope();
+
   ScopePtr current_scope() {
     return current_scope_;
   }
@@ -1322,7 +1323,7 @@ struct TORCH_API PythonOp : public Node {
   virtual void lint_python() const = 0;
 };
 
-TORCH_API void LintGraph(std::shared_ptr<Graph>& graph);
+TORCH_API void LintGraph(const std::shared_ptr<Graph>& graph);
 
 TORCH_API at::ArrayRef<Value*> createTupleUnpack(Value* v);
 
